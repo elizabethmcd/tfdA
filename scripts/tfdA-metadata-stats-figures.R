42abebc771acb3e01fbba763d7292f5bb15a8fc5
Elizabeth McDaniel <emcdaniel@wisc.edu>
2020-08-26 23:16:12 -0500

    reorg and script update

--- scripts/tfdA-metadata-stats-figures.R
+++ scripts/tfdA-metadata-stats-figures.R
@@ -1,17 +1,36 @@
-library(tidyverse)
 library(plyr)
+library(reshape2)
+library(tidyverse)
+library(RColorBrewer)
 
-metadata = read.csv("/Users/emcdaniel/Desktop/McMahon-Lab/tfdA/data/metadata/all-tfdA-metadata.csv")
+metadata = read.csv("/Users/emcdaniel/Desktop/McMahon-Lab/tfdA/metadata/hits-metadata/tfdA-all-genomes-metadata.csv")
 
-environment = count(metadata, "environment")
-group = count(metadata, "group")
+# pull out the Bart and Weigand data sets, because did large-scale studies of human associated bordetella isolates with long read sequencing, skews the plots
+filtered = metadata %>% filter(reference != 'Bart study' & reference != 'Weigand study')
+
+# frequency counts of environment and phylogenetic group for each genome
+environment = count(filtered, "source")
+group = count(filtered, "group")
 
 # Environments plots
-envColfunc = colorRampPalette(c("palegreen3", "slateblue"))
-bar_env = environment %>% ggplot(aes(x=reorder(environment, freq), y=freq)) + geom_bar(stat="identity", fill=(envColfunc(9))) + coord_flip() + scale_y_continuous(limits=c(0,700), expand = c(0, 0)) + theme(panel.grid.major = element_blank(), panel.grid.minor = element_blank(),panel.background = element_blank())
+longEnvs = spread(environment, source, freq)
+longEnvs$built_env_eng = longEnvs$built_environment + longEnvs$engineered
+longEnvs$unknown_isolates = longEnvs$unknown + longEnvs$isolate
+envCounts = longEnvs %>% select(animal_associated, freshwater, human_associated, marine, plant_associated, subsurface, symbiont, terrestrial, wetlands, built_env_eng, unknown_isolates)
+envList = gather(envCounts, "source", "freq")
+
+bar_env = envList %>% ggplot(aes(x=reorder(source, freq), y=freq)) + geom_col(aes(fill=freq)) + scale_fill_gradient(low="cornsilk", high="#003c30") + coord_flip() + scale_y_continuous(limits=c(0,375), expand = c(0, 0)) + theme(panel.grid.major = element_blank(), panel.grid.minor = element_blank(),panel.background = element_blank())
 bar_env
+ggsave(plot=bar_env, filename='figs/2020-08-26-tfdA-envs-plot.png', height=10, width=20, units=c('cm'))
 
 # Phyla/groups plot
-phylaColfunc = colorRampPalette(c("khaki1", "orchid4"))
-bar_phyla = group %>% ggplot(aes(x=reorder(group, freq), y=freq)) + geom_bar(stat="identity", fill=(phylaColfunc(16))) + coord_flip() + scale_y_continuous(limits=c(0,700), expand= c(0,0)) + theme(panel.grid.major = element_blank(), panel.grid.minor = element_blank(),panel.background = element_blank())
+longPhyla = spread(group, group,freq)
+longPhyla$other =  longPhyla$`Candidatus Latescibacteria` + longPhyla$Unclassified + longPhyla$Gemmatimonadetes + longPhyla$Acidobacteria + longPhyla$`Candidatus Tectomicrobia` + longPhyla$`PVC Superphylum` + longPhyla$Proteobacteria + longPhyla$Deltaproteobacteria + longPhyla$`Candidatus Eremiobacteraeota`
+phylaCounts = longPhyla %>% select(Alphaproteobacteria, Betaproteobacteria, Actinobacteria, `Candidatus Rokubacteria`, Gammaproteobacteria, other)
+phylaList = gather(phylaCounts, "group", "freq")
+
+bar_phyla = phylaList %>% ggplot(aes(x=reorder(group, freq), y=freq)) + geom_col(aes(fill=freq)) + scale_fill_gradient(low="rosybrown2", high="navyblue") + coord_flip() + scale_y_continuous(limits=c(0,650), expand = c(0, 0)) + theme(panel.grid.major = element_blank(), panel.grid.minor = element_blank(),panel.background = element_blank())
 bar_phyla
+
+# select freshwater
+freshwater = filtered %>% filter(source == 'freshwater')
